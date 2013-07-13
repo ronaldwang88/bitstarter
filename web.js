36099e8b3774ff0d936fa3c1c4af52edf207e8b2
Ubuntu <ubuntu@ip-10-164-31-154.ec2.internal>
2013-07-13 01:32:32 +0000

    web.js update 3

--- web.js
+++ web.js
@@ -3,12 +3,13 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 
-var text = fs.readFileSync('index.html');
-var answer = text.toString();
-
-
+var fs = require('fs');
 
 app.get('/', function(request, response) {
+
+  var text = fs.readFileSync('index.html');
+  var answer = text.toString();
+	
   response.send(answer);
 });
 
