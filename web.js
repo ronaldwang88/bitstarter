df480fa6ae77ed4862d70024c33360c7eb9078c6
Ubuntu <ubuntu@ip-10-164-31-154.ec2.internal>
2013-07-13 01:25:27 +0000

    web.js update 2

--- web.js
+++ web.js
@@ -2,8 +2,14 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+
+var text = fs.readFileSync('index.html');
+var answer = text.toString();
+
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(answer);
 });
 
 var port = process.env.PORT || 5000;
